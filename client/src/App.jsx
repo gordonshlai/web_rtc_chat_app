c93aa138fe42d85074b4aea71d112eaf6cf3174d
gordonshlai <gordonshlai@yahoo.com.hk>
2021-10-24 01:02:41 +0100

    Added components backbone

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Typography, AppBar } from "@material-ui/core";
+import VideoPlayer from "./components/VideoPlayer";
+import Notifications from "./components/Notifications";
+import Options from "./components/Options";
 
 function App() {
-  return <div>Video Chat App</div>;
+  return (
+    <div>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </div>
+  );
 }
 
 export default App;
